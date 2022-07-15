b623abf49e71d21df5889264db55df3b093beca5
Nelson Florez <nelsonflorez11@gmail.com>
2022-07-15 11:20:48 -0500

    agrega mejoras al localStorage

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 let produCarrito = []
 let precioTotalCompra = 0
 let productosTotalCompra = 0
-let productosBuscador = []
+let productosBuscador = " "
+let miStorage = window.localStorage;
 let sidebarComponentes = document.getElementById("sidebar")
 let botonCarrito = document.getElementById("carrito")
 let carritoVaciar = document.getElementById("carritoVaciar")
+
+
+const cargarCarritoLocal = () => {
+
+    if (miStorage.getItem("carrito") !== null){
+        produCarrito = JSON.parse(miStorage.getItem("carrito"))
+    }
+    if (miStorage.getItem("numeroPedidos") !== null){
+        productosTotalCompra = JSON.parse(miStorage.getItem("numeroPedidos"))
+        productos.innerText = productosTotalCompra
+    } 
+
+}
 
 
 //calculaTotalesGeneral se encarga de calcular el numero total de productos agregados y el total del precio
@@ -14,6 +28,7 @@ const calculaTotalesGeneral = () => {
     productosTotalCompra++
     let productos = document.getElementById("productos")    
     productos.innerText = productosTotalCompra
+    localStorage.setItem('numeroPedidos', JSON.stringify(productosTotalCompra)) 
     
 }
 
@@ -34,42 +49,49 @@ const calculaTotales = () => {
             })
     }
     //crea sessionStorage con los  productos que el usuario selecciona en el carrito, se limpian si se cierra el navegador
-    sessionStorage.setItem('carrito', JSON.stringify(produCarrito)) 
+    localStorage.setItem('carrito', JSON.stringify(produCarrito)) 
 
 }
 
-//crea la pantilla mostrando los productos
-let test = ''
-let contenido = document.getElementById("productos-contenido")
-for (const verProductos of productosCarrito){
-    
-    test +=  `
-            <div class="card text-center" style="width: 18rem;">
-            <img src="${verProductos.imagen}" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">${verProductos.producto}</h5>
-            <p class="card-text">${verProductos.gramos}.</p>
-            <p class="card-text">Precio: ${verProductos.precio}.</p>            
-            <button type="button" class="btn btn-success" data-id=${verProductos.id} >Agregar</button>
-            </div>
-            </div>
-            
-            `       
+//plantilla se encarga de crear la pantilla para mostrar los productos
+const plantilla = () => {
+    let test = ''
+    let contenido = document.getElementById("productos-contenido")
+    for (const verProductos of productosCarrito){
+        
+        test +=  `
+                <div class="card text-center" style="width: 18rem;">
+                <img src="${verProductos.imagen}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${verProductos.producto}</h5>
+                <p class="card-text">${verProductos.gramos}.</p>
+                <p class="card-text">Precio: ${verProductos.precio}.</p>            
+                <button type="button" class="btn btn-success" data-id=${verProductos.id} >Agregar</button>
+                </div>
+                </div>
+                
+                `       
+    }
+    contenido.innerHTML = test    
+
 }
-contenido.innerHTML = test
 
 //agrega el evento click al boton de los productos
-const boton = document.querySelectorAll('button')
+const eventoBoton = () => {
+    
+    const boton = document.querySelectorAll('button')
     boton.forEach(function(item){
         item.addEventListener('click', function(){            
             productosBuscador = productosCarrito.find(productosCarrito => productosCarrito.id == item.dataset.id)
-            calculaTotalesGeneral(productosBuscador)
+            calculaTotalesGeneral()
             calculaTotales()        
          
-    })
-    
+        })    
 
-})
+    })   
+
+}
+
 
 //carritoS muestra los productos en el carrito y la cantidad
 carritoS.onclick = () => {
@@ -92,6 +114,12 @@ carritoS.onclick = () => {
 carritoVaciar.onclick = () => {
     produCarrito = []
     precioTotalCompra = 0
-    productosTotalCompra = 0    
+    productosTotalCompra = 0
+    localStorage.clear();  
     location.reload()
 }
+
+// Inicio
+cargarCarritoLocal()
+plantilla()
+eventoBoton()
